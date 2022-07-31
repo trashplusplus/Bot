6f39c4167393d8e8b96382a8daaa21836c53c779
trashplusplus <willoxvlad@gmail.com>
2022-07-31 22:22:50 +0300

    design emojis

--- src/main/java/Casino.java
+++ src/main/java/Casino.java
@@ -1,24 +1,23 @@
 
 public class Casino {
 
-    private int bid;
+    private int dash;
     private long coefficientX;
 
     Casino(){
-        this.bid = 0;
+        this.dash = 0;
     }
 
-    public void newBid(int playerBid, long coefficientX){
-        this.bid = bid;
+    public void newDash(int playerBid, long coefficientX){
+        this.dash = dash;
         this.coefficientX = coefficientX;
     }
 
-
     public String youLose(Player player){
-        return player.getUsername().toString() + " проиграл со ставкой " + bid * coefficientX;
+        return player.getUsername().toString() + " проиграл со ставкой " + dash * coefficientX;
     }
 
     public String youWon(Player player){
-        return player.getUsername().toString() + " выиграл со ставкой " + bid * coefficientX;
+        return player.getUsername().toString() + " выиграл со ставкой " + dash * coefficientX;
     }
 }
