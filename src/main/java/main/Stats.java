65ad4c7a66a6395c23d8dc9c33042f8e32a4a0ea
trashplusplus <willoxvlad@gmail.com>
2022-08-20 17:17:56 +0300

    Update Stats.java

--- src/main/java/main/Stats.java
+++ src/main/java/main/Stats.java
@@ -5,5 +5,9 @@ import java.util.List;
 
 public class Stats {
     public boolean bonus = false;
+    public int cupsOfCoffee = 0;
+    public int cupsOfTee = 0;
+
+    public List<String> plants = new ArrayList<String>();
 
 }
