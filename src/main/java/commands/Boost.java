1942845f77fdfaffdb27ccfc59736951621c6895
Homo97Sapiens <>
2022-09-18 02:14:12 +0600

    commands wip

---
+++ src/main/java/commands/Boost.java
@@ -0,0 +1,22 @@
+package commands;
+
+import main.Bot;
+import main.Player;
+
+public class Boost extends Command
+{
+	@Override
+	public void consume(Bot host, Player player)
+	{
+		if (player.needle >= 2)
+		{
+			host.sendMsg(player.getId(), "\uD83D\uDC8E Вы сбросили время ожидания");
+			player.needle -= 2;
+			player.findExpiration = 0L;
+		}
+		else
+		{
+			host.sendMsg(player.getId(), "\uD83D\uDC8E Недостаточно булавок. Чтобы купить их введите команду /donate");
+		}
+	}
+}
